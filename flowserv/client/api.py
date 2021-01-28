16fe9409db75d307c67187fa789c1acd9fa4933f
Heiko Mueller <heiko.muller@gmail.com>
2021-01-28 16:05:57 -0500

    Update documentation

--- flowserv/client/api.py
+++ flowserv/client/api.py
@@ -45,6 +45,8 @@ def ClientAPI(
     basedir: string, default=None
         Base directory for all workflow files. If no directory is given or
         specified in the environment a temporary directory will be created.
+    database: string, default=None
+        Optional database connect url.
     open_access: bool, default=None
         Use an open access policy if set to True.
     run_async: bool, default=False
