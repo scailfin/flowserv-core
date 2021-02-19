b948d9def0e0acf92fc3594e3c5ba9c1e4a3eb3f
Heiko Mueller <heiko.muller@gmail.com>
2021-02-19 05:35:26 -0500

    Add serial workflow run result

--- flowserv/controller/serial/workflow/result.py
+++ flowserv/controller/serial/workflow/result.py
@@ -9,7 +9,9 @@
 """Workflow (step) execution result."""
 
 from dataclasses import dataclass, field
-from typing import List, Optional
+from typing import Any, Dict, List, Optional, Tuple
+
+from flowserv.controller.serial.workflow.step import WorkflowStep
 
 
 @dataclass
@@ -24,3 +26,125 @@ class ExecResult:
     stdout: Optional[List[str]] = field(default_factory=list)
     stderr: Optional[List[str]] = field(default_factory=list)
     exception: Optional[Exception] = None
+
+
+class RunResult(object):
+    """Result for a serial workflow run. For each executed workflow step the
+    run result maintains the step itself and the :class:ExecResult. In addition,
+    the run result maintains the context of the workflow that is modified by the
+    executed workflow steps.
+
+    Provides properties for easy access to the return code of the final workflow
+    step and the outputs to STDOUT and STDERR.
+    """
+    def __init__(self, arguments: Dict):
+        """Initialize the run context with the initial set of user-provided
+        arguments for the workflow run.
+
+        Parameters
+        ----------
+        arguments: dict
+            Dictionary of user-provided input arguments for the workflow run.
+        """
+        self.context = dict(arguments)
+        self.steps = list()
+
+    def __len__(self) -> int:
+        """Get number of executed workflow steps.
+
+        Returns
+        -------
+        int
+        """
+        return len(self.steps)
+
+    def add(self, step: WorkflowStep, result: ExecResult):
+        """Add execution result for a workflow step.
+
+        Parameters
+        ----------
+        step: flowserv.controller.serial.workflow.step.WorkflowStep
+            Executed workflow step.
+        result: flowserv.controller.serial.workflow.result.ExecResult
+            Execution result for the workflow step.
+        """
+        self.steps.append((step, result))
+
+    @property
+    def exception(self) -> Exception:
+        """Get the exception from the last workflow step that was executed in
+        the workflow run.
+
+        The result is None if no workflow step has been executed yet.
+
+        Returns
+        -------
+        Exception
+        """
+        return self.steps[-1][1].exception if self.steps else None
+
+    def get(self, var: str) -> Any:
+        """Get the value for a given variable from the run context.
+
+        Raises a KeyError if the variable is not defined.
+
+        Parameters
+        ----------
+        var: string
+            Variable name
+
+        Returns
+        -------
+        any
+        """
+        return self.context[var]
+
+    @property
+    def returncode(self) -> int:
+        """Get the return code from the last workflow step that was executed in
+        the workflow run.
+
+        The result is None if no workflow step has been executed yet.
+
+        Returns
+        -------
+        int
+        """
+        return self.steps[-1][1].returncode if self.steps else None
+
+    @property
+    def stderr(self) -> List[str]:
+        """Get all lines that were written to STDERR by all executed workflow
+        steps.
+
+        Returns
+        -------
+        list of string
+        """
+        return [line for _, result in self.steps for line in result.stderr]
+
+    @property
+    def stdout(self) -> List[str]:
+        """Get all lines that were written to STDOUT by all executed workflow
+        steps.
+
+        Returns
+        -------
+        list of string
+        """
+        return [line for _, result in self.steps for line in result.stdout]
+
+    def step(self, index: int) -> Tuple[WorkflowStep, ExecResult]:
+        """Get step sdefinition and execution result for the workflow step at
+        the given index position.
+
+        Parameters
+        ----------
+        index: int
+            Index position for an executed workflow run step.
+
+        Returns
+        -------
+        tuple of WorkflowStep, ExecResult
+        """
+        return self.steps[index]
---
+++ tests/controller/serial/workflow/test_run_result.py
@@ -0,0 +1,57 @@
+# This file is part of the Reproducible and Reusable Data Analysis Workflow
+# Server (flowServ).
+#
+# Copyright (C) 2019-2021 NYU.
+#
+# flowServ is free software; you can redistribute it and/or modify it under the
+# terms of the MIT License; see LICENSE file for more details.
+
+"""Unit tests for the serial workflow run result class."""
+
+from flowserv.controller.serial.workflow.result import ExecResult, RunResult
+from flowserv.controller.serial.workflow.step import ContainerStep
+
+
+def test_empty_run_result():
+    """Test run result properties for a run without any executed workflow steps."""
+    r = RunResult(arguments={'a': 1})
+    assert len(r) == 0
+    assert r.get('a') == 1
+    assert r.exception is None
+    assert r.returncode is None
+    assert r.stderr == []
+    assert r.stdout == []
+
+
+def test_error_run_result():
+    """Test results of an erroneous workflow run."""
+    r = RunResult(arguments={})
+    r.add(ContainerStep(image='test'), ExecResult(returncode=0))
+    assert r.exception is None
+    assert r.returncode == 0
+    r.add(ContainerStep(image='test'), ExecResult(returncode=1, stderr=['e1', 'e2'], exception=ValueError()))
+    assert r.exception is not None
+    assert r.returncode == 1
+    assert r.stdout == []
+    assert r.stderr == ['e1', 'e2']
+
+
+def test_successful_run_result():
+    """Test results of a successful workflow run."""
+    r = RunResult(arguments={'a': 1})
+    r.add(ContainerStep(image='test1'), ExecResult(returncode=0, stdout=['o1']))
+    r.context['a'] = 2
+    r.add(ContainerStep(image='test2'), ExecResult(returncode=0, stdout=['o2', 'o3']))
+    r.context['b'] = 1
+    assert r.exception is None
+    assert r.returncode == 0
+    assert r.stdout == ['o1', 'o2', 'o3']
+    assert r.stderr == []
+    assert r.get('a') == 2
+    assert r.get('b') == 1
+    step, result = r.step(0)
+    assert step.image == 'test1'
+    assert result.stdout == ['o1']
+    step, result = r.step(1)
+    assert step.image == 'test2'
+    assert result.stdout == ['o2', 'o3']
