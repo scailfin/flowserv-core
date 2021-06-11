ea9130d9afdae1a334666d912fef6c7d10b4fc26
Heiko Mueller <heiko.muller@gmail.com>
2021-06-11 09:13:06 -0400

    Update dev version

--- flowserv/version.py
+++ flowserv/version.py
@@ -7,4 +7,4 @@
 # terms of the MIT License; see LICENSE file for more details.
 
 """Information about the current version of the flowServ package."""
-__version__ = '0.9.0.dev_notebook'
+__version__ = '0.9.0.dev_gui'
